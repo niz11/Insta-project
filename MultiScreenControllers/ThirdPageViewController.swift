933fffefd9d23024ed8650c75db3bed71e8fb486
niz <nizan@nizans-mbp.speedport.ip>
2019-05-29 19:01:10 +0200

    Working on third page

--- MultiScreenControllers/ThirdPageViewController.swift
+++ MultiScreenControllers/ThirdPageViewController.swift
@@ -10,11 +10,62 @@ import UIKit
 
 class ThirdPageViewController: UIViewController {
 
+    @IBOutlet weak var FullNameButton: UITextField!
+    @IBOutlet weak var PasswordButton: UITextField!
+    
+    @IBOutlet weak var ContinueAndSync: UIButton!
+    
+    var lengthOfUserName = 0
+    var lengthOfUPassword = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //Changing style of buttons
+        FullNameButton.translatesAutoresizingMaskIntoConstraints = false
+        FullNameButton.placeholder = "Full Name"
+        FullNameButton.borderStyle = .roundedRect
+        FullNameButton.font = .systemFont(ofSize: 14)
+        FullNameButton.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        
+        PasswordButton.translatesAutoresizingMaskIntoConstraints = false
+        PasswordButton.placeholder = "Password"
+        PasswordButton.isSecureTextEntry = true
+        PasswordButton.borderStyle = .roundedRect
+        PasswordButton.font = .systemFont(ofSize: 14)
+        PasswordButton.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        
+        
     }
+    
+
+    @IBAction func OnClickName(_ sender: Any) {
+        print("HEllo2")
+    }
+    
+    
+    @IBAction func OnTypeName(_ sender: Any) {
+        lengthOfUserName = String(FullNameButton.text!).count
+        
+        if lengthOfUserName > 3 && lengthOfUPassword > 3{
+            ContinueAndSync.alpha = 1
+        } else {
+            ContinueAndSync.alpha = 0.6
+        }
+    }
+    
+    @IBAction func OnTouchPass(_ sender: Any) {
+        print("HEllo2")
+    }
+    
+    @IBAction func OnTypePass(_ sender: Any) {
+        lengthOfUPassword = String(PasswordButton.text!).count
+        
+        if lengthOfUserName > 3 && lengthOfUPassword > 3{
+            ContinueAndSync.alpha = 1
+        } else {
+            ContinueAndSync.alpha = 0.6
+        }
+    }
+    
     
 
 }
