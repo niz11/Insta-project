fa7f3f8b31cb371da5e90615e66aa133583a536f
niz <nizan@nizans-mbp.speedport.ip>
2019-06-10 17:53:11 +0200

    saving before changing all to iphone 8 layout

--- SecondLook/tenthViewController.swift
+++ SecondLook/tenthViewController.swift
@@ -5,26 +5,21 @@
 //  Created by Nizan Goldstein on 05.06.19.
 //  Copyright © 2019 Nizan Goldstein. All rights reserved.
 //
+// last page - record a video
 
 import UIKit
+import MobileCoreServices
+
 
 class tenthViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func record(_ sender: Any) {
     }
-    */
-
 }
+
